b3c4e3e1e12e8fd2f489e1d066d09c43300bd919
Steven <Steven.h6886@gmail.com>
2025-07-31 00:35:07 -0400

    types and moves csv created

--- backend/database/populate.py
+++ backend/database/populate.py
@@ -15,6 +15,11 @@ def loadList(fileName):
         loadedList = [line.strip() for line in file]
     return loadedList
 
+def create_csv(dictionary, csvName, index_label):
+    df = pd.DataFrame(dictionary)
+    df.index = df.index+1
+    return df.to_csv(csvName, index_label=index_label)
+
 def items_csv():
     item_url = BASE_URL + "/item/"
     items = []
@@ -67,13 +72,65 @@ def items_csv():
     # saveList("categories.txt", categories)
     # saveList("items.txt", items)
 
-    print(items)
-    print('----')
-    print(categories)
+def types_csv():
+    types_url = BASE_URL + "/type/"
+    response = requests.get(types_url)
+    data = response.json()
+    types = []
+    for idx, i in enumerate(data['results']):
+        types.append(i['name'])
+    
+    print(types)
+    types_dict = {
+        "type_name" : types
+    }
 
+    create_csv(types_dict, 'types.csv', index_label='type_id')
+
+def moves_csv():
+    moves_url = BASE_URL + "/move/"
+    response = requests.get(moves_url)
+    data = response.json()
+    num_moves = data['count']
+    type_id = []
+    move_name = []
+    base_power = []
+    Accuracy = []
+    damage_type = []
+
+    df = pd.read_csv("types.csv")
+    for i in range(num_moves):
+        move_url = moves_url + str(i+1)
+        response = requests.get(move_url)
+        if response.status_code == 200:
+            data = response.json()
+            move_name.append(data['name'])
+            if data['power'] == "None":
+                base_power.append("NULL")
+            else:
+                base_power.append(data['power'])
+            if data['accuracy'] == "None":
+                Accuracy.append("NULL")
+            else:
+                Accuracy.append(data['accuracy'])
+            damage_type.append(data['damage_class']['name'])
+            type_id.append(df.loc[df["type_name"] == data['type']['name']]['type_id'].values[0])
+
+        else:
+            print(f'move {i} not found')
+
+    move_dictionary = {
+        "type_id" : type_id,
+        "move_name": move_name,
+        "base_power": base_power,
+        "Accuracy": Accuracy,
+        "damage_type": damage_type
+    }
+
+    create_csv(move_dictionary, "move.csv", "move_id")
 
 def main():
-    items_csv()
+    print("main")
 
 if __name__=="__main__":
     main()
